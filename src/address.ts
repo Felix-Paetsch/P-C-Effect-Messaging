39b5e1b519fb3549fc3c4fa731409434308ac6b4
Ellyly <felixpaetsch@gmail.com>
2025-05-23 12:02:08 +0200

    Addresses

--- src/address.ts
+++ src/address.ts
@@ -1,31 +1,40 @@
 import { UUID } from "./utils/uuid";
-import { Effect } from "effect";
+import { Data, Effect, ParseResult, Schema, Equal, Hash } from "effect";
 
-export class DeserializationError extends Error {
-    _tag = "@msg/address/DeserializationError";
-}
+export class DeserializationError extends Data.TaggedError("DeserializationError")<{}> { }
 
-export default class Address {
+export default class Address implements Equal.Equal {
     constructor(
         public readonly host_id: UUID,
         public readonly plugin_id: UUID
     ) { }
 
-    equals(other: Address) {
-        return this.host_id === other.host_id && this.plugin_id === other.plugin_id;
-    }
-
-    serialize() {
-        return `HOST_ID: ${this.host_id}\nPLUGIN_ID: ${this.plugin_id}`
-    }
-
-    static deserialize = (s: string): Effect.Effect<Address, DeserializationError, never> => {
-        const lines = s.split("\n");
-        const host_id = lines[0].split(": ")[1];
-        const plugin_id = lines[1].split(": ")[1];
-        if (!host_id || !plugin_id) {
-            return Effect.fail(new DeserializationError("Invalid address format"));
+    [Equal.symbol](that: Equal.Equal): boolean {
+        if (that instanceof Address) {
+            return (
+                Equal.equals(this.host_id, that.host_id) &&
+                Equal.equals(this.plugin_id, that.plugin_id)
+            )
         }
-        return Effect.succeed(new Address(host_id, plugin_id));
+
+        return false
     }
+
+    [Hash.symbol](): number {
+        return Hash.hash(this.plugin_id)
+    }
+
+    static AddressFromString = Schema.transformOrFail(Schema.String, Schema.instanceOf(Address), {
+        decode: (str: string, _, ast) => {
+            const lines = str.split("\n");
+            const host_id = lines[0].split(": ")[1];
+            const plugin_id = lines[1].split(": ")[1];
+            if (!host_id || !plugin_id) {
+                return ParseResult.fail(new ParseResult.Type(ast, str, "Failed to deserialize address"));
+            }
+            return Effect.succeed(new Address(host_id, plugin_id));
+        },
+        encode: (address: Address) =>
+            ParseResult.succeed(`HOST_ID: ${address.host_id}\nPLUGIN_ID: ${address.plugin_id}`)
+    })
 }
